f9ac062410a198e6b124c6d32b3e765ebea778ab
Mateus Alves de Oliveira <devmateusalves@gmail.com>
2021-07-04 17:56:29 -0300

    file medium and file low

--- src/asset/models.py
+++ src/asset/models.py
@@ -14,11 +14,23 @@ class Asset(models.Model):
         blank=False,
         null=False,
     )
+    file_medium = models.FileField(
+        upload_to=upload_directory_path,
+        blank=True,
+        null=True,
+    )
+    file_low = models.FileField(
+        upload_to=upload_directory_path,
+        blank=True,
+        null=True,
+    )
     uploaded_at = models.DateTimeField('Uploaded at', auto_now_add=True)
 
     def __str__(self):
         return f'Image {self.id}: <{self.file}>'
-    
+
     def save(self, *args, **kwargs):
-        self.file = resize_and_crop_file(self.file)
+        self.file = resize_and_crop_file(self.file, 1)
+        self.file_medium = resize_and_crop_file(self.file, 2)
+        self.file_low = resize_and_crop_file(self.file, 3)
         super(Asset, self).save(*args, **kwargs)
